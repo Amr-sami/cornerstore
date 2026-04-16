6b58653eff3fca7a4971322a59419f9c367859d9
Amr-sami <samyamr819@gmail.com>
2026-04-16 23:16:43 +0200

    Fix blank print page issue by using visibility-based print strategy

--- components/layout/AppShell.tsx
+++ components/layout/AppShell.tsx
@@ -14,18 +14,17 @@ export function AppShell({ children, title }: AppShellProps) {
   return (
     <div className="min-h-screen bg-bg-main overflow-x-hidden">
       {/* Desktop Sidebar */}
-      <div className="hidden lg:block fixed start-0 top-0 w-60 h-screen bg-bg-sidebar z-40">
+      <div className="hidden lg:block fixed start-0 top-0 w-60 h-screen bg-bg-sidebar z-40 no-print">
         <Sidebar />
       </div>
 
       {/* Main Content Area */}
       <div className="lg:ms-60 min-h-screen flex flex-col">
-        <Header title={title} />
         <main className="flex-1 p-4 md:p-6 pb-24 lg:pb-6">{children}</main>
       </div>
 
       {/* Mobile Bottom Nav */}
-      <div className="lg:hidden fixed bottom-0 inset-x-0 z-50">
+      <div className="lg:hidden fixed bottom-0 inset-x-0 z-50 no-print">
         <MobileBottomNav />
       </div>
     </div>
